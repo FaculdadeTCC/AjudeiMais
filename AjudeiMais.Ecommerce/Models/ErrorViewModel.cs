e3a2c784aa3e8eb0e2e072321a8e7e36972ae669
Luiz Matheus <luizmatheus.profissional@outlook.com>
2025-04-04 15:44:14 -0300

    Adicionar arquivos de projeto.

---
+++ AjudeiMais.Ecommerce/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AjudeiMais.Ecommerce.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
