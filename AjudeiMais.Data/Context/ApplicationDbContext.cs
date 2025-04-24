2e34807785c1ffe02db2f34cd1c2f70d76f4e64c
luiz-matheus-da-silva <luizmatheus.profissional@hotmail.com>
2025-04-24 20:55:34 -0300

    fix: Criação dos Modelos do Banco de Dados

---
+++ AjudeiMais.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AjudeiMais.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace AjudeiMais.Data.Context
+{
+    internal class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :
+            base(options){ }
+
+        public DbSet<Usuario> Usuario { get; set; }
+    }
+}
