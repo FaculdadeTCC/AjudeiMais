3e164302d7185303b8ff913070b893f1dad3936c
Danilo derio <116591285+Eidaniloderio@users.noreply.github.com>
2025-04-24 21:32:12 -0300

    Feat: Criação dos modelos de banco de dados

---
+++ AjudeiMais.Data/Models/Chat.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AjudeiMais.Data.Models
+{
+    public class Chat
+    {
+        [Key]
+        public int Chat_ID{ get; set; }
+        public DateTime DataAbertura { get; set; }
+        public bool Habilitado { get; set; }
+        public bool Excluido { get; set; }
+        public string GUID { get; set; }
+
+        [ForeignKey("Usuario")]
+        public int Usuario_ID { get; set; }
+        public Usuario Usuario { get; set; }
+
+        [ForeignKey("Instituicao")]
+        public int Instituicao_ID { get; set; }
+        public Instituicao Instituicao { get; set; }
+
+
+    }
+}
---
+++ AjudeiMais.Data/Models/InstituicaoCategoria.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AjudeiMais.Data.Models
+{
+    public class InstituicaoCategoria
+    {
+        public int InstituicaoCategoria_ID { get; set; }
+
+        public int Instituicao_ID { get; set; }
+        public Instituicao Instituicao { get; set; }
+
+        public int Categoria_ID { get; set; }
+        public Categoria Categoria { get; set; }
+
+    }
+}
