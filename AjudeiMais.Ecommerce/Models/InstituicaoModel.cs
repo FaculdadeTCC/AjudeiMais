b17a36404a6634cb549bbf3b80d1e94167648788
Danilo derio <116591285+Eidaniloderio@users.noreply.github.com>
2025-05-22 23:30:50 -0300

    fex: merge com a master

---
+++ AjudeiMais.Ecommerce/Models/InstituicaoModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AjudeiMais.Ecommerce.Models
+{
+    public class InstituicaoModel
+    {
+        public class Instituicao
+        {
+            [Key]
+            public int Instituicao_ID { get; set; }
+            public string Nome { get; set; }
+            public string Descricao { get; set; }
+            public string Telefone { get; set; }
+            public string Email { get; set; }
+            public string Senha { get; set; }
+            public string? Guid { get; set; }
+            public string? Role { get; set; }
+            public string? Avaliacao { get; set; }
+            public DateTime DataCriacao { get; set; }
+            public DateTime DataUpdate { get; set; }
+        }
+    }
+}
