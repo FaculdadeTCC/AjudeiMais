178febe84bc1eb8ac2986238a7fcb2af6ce80281
Danilo derio <116591285+Eidaniloderio@users.noreply.github.com>
2025-05-25 17:14:50 -0300

    Feat: integração do cadastro de instituição com a view e api

--- AjudeiMais.API/DTO/InstituicaoDTO.cs
+++ AjudeiMais.API/DTO/InstituicaoDTO.cs
@@ -1,17 +1,15 @@
 ﻿using AjudeiMais.API.DTO;
+using Microsoft.AspNetCore.Mvc;
 
 public class InstituicaoDTO
 {
     public int Instituicao_ID { get; set; }
     public string Nome { get; set; }
-    public string CNPJ { get; set; }
     public string Email { get; set; }
     public string Senha { get; set; }
-
     public string Telefone { get; set; }
-    public string GUID { get; set; }
 
+    [ModelBinder(BinderType = typeof(FromJsonBinder<List<EnderecoDTO>>))]
     public List<EnderecoDTO> Enderecos { get; set; }
 
-    public IFormFile[] Imagens { get; set; } // múltiplas imagens
 }
