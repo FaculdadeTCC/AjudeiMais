90994b0bde97a6f11e064b4f97f70b1494fcc39c
Danilo derio <116591285+Eidaniloderio@users.noreply.github.com>
2025-05-25 16:02:41 -0300

    puxando master pro dev-danilo

---
+++ AjudeiMais.API/Tools/FromJsonBinder.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Newtonsoft.Json;
+
+public class FromJsonBinder<T> : IModelBinder
+{
+    public Task BindModelAsync(ModelBindingContext bindingContext)
+    {
+        var value = bindingContext.ValueProvider.GetValue(bindingContext.ModelName).FirstValue;
+
+        if (string.IsNullOrEmpty(value))
+        {
+            bindingContext.Result = ModelBindingResult.Success(default(T));
+            return Task.CompletedTask;
+        }
+
+        try
+        {
+            var result = JsonConvert.DeserializeObject<T>(value);
+            bindingContext.Result = ModelBindingResult.Success(result);
+        }
+        catch (Exception)
+        {
+            bindingContext.ModelState.AddModelError(bindingContext.ModelName, "Invalid JSON.");
+        }
+
+        return Task.CompletedTask;
+    }
+}
