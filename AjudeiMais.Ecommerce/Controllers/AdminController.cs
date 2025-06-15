ed7504fba342bcaaec509ed6aed6f25f79fa3d5e
luiz-matheus-da-silva <luizmatheus.profissional@gmail.com>
2025-06-15 14:43:30 -0300

    merge

--- AjudeiMais.Ecommerce/Controllers/AdminController.cs
+++ AjudeiMais.Ecommerce/Controllers/AdminController.cs
@@ -24,7 +24,7 @@ namespace AjudeiMais.Ecommerce.Controllers
         public async Task<IActionResult> IndexAsync()
         {
             List<UsuarioPerfilModel> usuarios = new List<UsuarioPerfilModel>();
-            List<InstituicaoPerfilModel> instituicoes = new List<InstituicaoPerfilModel>();
+            List<InstituicaoModel> instituicoes = new List<InstituicaoModel>();
 
             string errorMessage = null;
 
